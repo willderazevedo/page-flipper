ae7b7277c5123f503fd9f220ca348db96b129393
Willder Azevedo <willderazevedo434@gmail.com>
2026-05-18 11:43:43 -0300

    Fix background image field

--- fields/flipper-background.php
+++ fields/flipper-background.php
@@ -44,7 +44,15 @@ function wa_page_flipper_background_meta_box_save( $post_id ) {
         return;
     }
 
-    if ( ! isset( $_POST['wa_page_flipper_background_nonce'] ) || ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['wa_page_flipper_background_nonce'] ) ), 'wa_page_flipper_background_nonce_action' ) ) {
+    if (
+        ! isset( $_POST['wa_page_flipper_background_nonce'] ) ||
+        ! wp_verify_nonce(
+            sanitize_text_field(
+                wp_unslash( $_POST['wa_page_flipper_background_nonce'] )
+            ),
+            'wa_page_flipper_background_nonce_action'
+        )
+    ) {
         return;
     }
 
@@ -52,26 +60,51 @@ function wa_page_flipper_background_meta_box_save( $post_id ) {
         return;
     }
 
-    if ( isset( $_POST['background_data'] ) ) {
-        $background_data_raw = wp_unslash( $_POST['background_data'] );
-
-        if ( is_string( $background_data_raw ) ) {
-            $background_data = json_decode( $background_data_raw, true );
-
-            if ( json_last_error() === JSON_ERROR_NONE && is_array( $background_data ) ) {
-                array_walk_recursive( $background_data, function ( &$value ) {
-                    if ( is_string( $value ) ) {
-                        $value = sanitize_text_field( $value );
-                    }
-                });
-
-                update_post_meta(
-                    $post_id,
-                    '_wa_page_flipper_background_data',
-                    wp_json_encode( $background_data, JSON_UNESCAPED_UNICODE )
-                );
-            }
-        }
+    if ( ! isset( $_POST['background_data'] ) ) {
+        return;
     }
+
+    $background_data_raw = wp_unslash( $_POST['background_data'] );
+
+    if ( ! is_string( $background_data_raw ) ) {
+        return;
+    }
+
+    $background_data = json_decode( $background_data_raw, true );
+
+    if ( json_last_error() !== JSON_ERROR_NONE ) {
+        return;
+    }
+
+    if ( $background_data === null ) {
+
+        delete_post_meta(
+            $post_id,
+            '_wa_page_flipper_background_data'
+        );
+
+        return;
+    }
+
+    if ( ! is_array( $background_data ) ) {
+        return;
+    }
+
+    array_walk_recursive( $background_data, function ( &$value ) {
+
+        if ( is_string( $value ) ) {
+            $value = sanitize_text_field( $value );
+        }
+
+    });
+
+    update_post_meta(
+        $post_id,
+        '_wa_page_flipper_background_data',
+        wp_json_encode(
+            $background_data,
+            JSON_UNESCAPED_UNICODE
+        )
+    );
 }
 add_action( 'save_post', 'wa_page_flipper_background_meta_box_save' );
